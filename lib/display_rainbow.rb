e65aee6e46038d46423017a6f336000551e4620d
sofia tofigh <sst2140@columbia.edu>
2020-02-03 21:53:03 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,4 +3,4 @@ def display_rainbow(color)
   print "R: #{color[0]}, O: #{color[1]}, Y: #{color[2]}, G: #{color[3]}, B: #{color[4]}, I: #{color[5]}, V: #{color[6]}"
 end
 
-display_rainbow(color)
+display_rainbow
